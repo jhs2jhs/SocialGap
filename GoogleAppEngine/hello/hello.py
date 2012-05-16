c0752f625f849224089cf12524cbeec905fa5558
jianhuashao <jianhua.shao1986@gmail.com>
2012-05-16 12:03:56 +0100

    Hello tutorial from google
    https://developers.google.com/appengine/docs/python/gettingstartedpython
    27/

---
+++ GoogleAppEngine/hello/hello.py
@@ -0,0 +1,36 @@
+import webapp2
+import cgi
+
+from google.appengine.api import users
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class FormPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("""
+          <html>
+            <body>
+              <form action="/sign" method="post">
+                <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                <div><input type="submit" value="Sign Guestbook"></div>
+              </form>
+            </body>
+          </html>""")
+
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/form', FormPage), ('/sign', Guestbook)],
+                              debug=True)
